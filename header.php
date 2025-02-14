a06506af7775285dd151251fc23dc69b0ac37918
Subash Chhetri <subash23000@student.hamk.fi>
2025-02-14 13:02:58 +0200

    add

--- header.php
+++ header.php
@@ -3,11 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+        <?php echo $title; ?>
+    </title>
+    <style>
+        
+    </style>
+
+
 </head>
 <body>
-    hello
-
-    
-</body>
-</html>
+<header> 
+        <div class="logo">
+            <img src="images/logo2.png" alt="Himalayan Cafe Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="proj.html">HOME</a></li>
+                <li><a href="menu.html">MENU</a></li>
+                <li><a href="#order">ORDER</a></li>
+                <li><a href="#locations">LOCATION</a></li>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#review">REVIEW</a></li>
+                <li><a href="#feedback">FEEDBACK</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+            </ul>
+        </nav>
+</header>
