87e701f07e620a79e8222709604520b9b915765d
Subash Chhetri <subash23000@student.hamk.fi>
2025-02-14 13:14:03 +0200

    done

--- header.php
+++ header.php
@@ -7,6 +7,43 @@
         <?php echo $title; ?>
     </title>
     <style>
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #ff8800;
+    padding: 15px 50px;
+    position: sticky;
+    top: -20px;
+    z-index: 1000;
+    
+}
+.logo img {
+    width: 50px;
+}
+nav ul {
+    list-style: none;
+    display: flex;
+    gap: 20px;
+}
+
+nav ul li {
+    display: inline;
+}
+
+nav ul li a {
+    text-decoration: none;
+    color: black;
+    font-weight: bolder;
+    transition: 0.3s;
+}
+nav ul li a:hover, 
+nav ul li a:active {
+    background: white;
+    padding: 8px 12px;
+    border-radius: 5px;
+}
         
     </style>
 
