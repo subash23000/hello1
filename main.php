24b003a87c251ca62cfe17c866f4f5ff154ae65a
Subash Chhetri <subash23000@student.hamk.fi>
2025-02-14 13:22:56 +0200

    added css for main

--- main.php
+++ main.php
@@ -4,7 +4,181 @@ $title = "My Title";
 include 'header.php';
 ?>
 <style>
-    
+.intro{
+    text-align: center;
+    padding: 50px 20px;
+}
+.intro img {
+    max-width: 30%;
+    border-radius: 10px;
+}
+.opening-hours {
+    padding-top: 10px;
+    margin: 0;
+}
+.opening-hours ul {
+    margin-top: 20px;
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+.welcome,.menu,.locations {
+    text-align: center;
+    padding: 50px 20px;
+} 
+.drinks {
+    display: flex;
+    justify-content: center;
+    gap: 30px;
+    flex-wrap: wrap;
+}
+.drink img {
+    width: 100px;
+    border-radius: 10px;
+    transition: transform 0.3s;
+}
+.drink img:hover{
+    width: 200px;
+    border-radius: 20px;
+    transform: scale(1.05);
+}
+button a {
+    text-decoration: none;
+}
+
+.order {
+    text-align: center;
+    padding: 50px 20px;
+    background: white;
+    border-top: 5px solid #ff8800;
+}
+.order h2 {
+    color: #ff8800;
+    font-size: 2rem;
+    margin-bottom: 10px;
+}
+.order p {
+    font-size: 1.2rem;
+    color: gray;
+    margin-bottom: 30px;
+}
+.order form {
+    max-width: 600px;
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 30px;
+    border-radius: 10px;
+}
+.form-group {
+    margin-bottom: 20px;
+    text-align: left;
+}
+.form-group label {
+    font-size: 1rem;
+    color: black;
+    display: block;
+    margin-bottom: 5px;
+}
+.form-group input,
+.form-group textarea {
+    width: 100%;
+    padding: 10px;
+    font-size: 1rem;
+    border: 1px solid grey;
+    border-radius: 5px;
+}
+.order-button, .button {
+    display: inline-block;
+    background-color: #ff8800;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    text-decoration: none;
+    font-weight: bold;
+    transition: 0.3s;
+}
+.menu .button {
+    text-decoration: none;
+}
+.order-button:hover{
+    background-color: black;
+    color: white;
+}
+.locations {
+    text-align: center;
+}
+.locations img {
+    width: 250px;
+    border-radius: 10px;
+}
+.location {
+    display: inline-block;
+    margin: 20px;
+
+}
+.location:hover,
+.location:active {
+    background-color: lightblue;
+    padding: 8px 12px;
+    border-radius: 5px;
+}
+.feedback {
+    text-align: center;
+    padding: 50px 20px;
+    background: white;
+}
+.feedback h2 {
+    color: #ff8800;
+    font-size: 2.5rem;
+    margin-bottom: 10px;
+}
+.feedback p {
+    font-size: 1.2rem;
+    margin-bottom: 20px;
+    color: gray;
+}
+.feedback form {
+    max-width: 600px;
+    margin: 0 auto;
+    background-color: white;
+    padding: 30px;
+    border-radius: 10px;
+
+}
+.feedback-button {
+    display: inline-block;
+    background: #ff8800;
+    color: #fff;
+    border: none;
+    padding: 15px 30px;
+    font-size: 1rem;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.feedback-button:hover {
+    background-color: black;
+    color: white;
+}
+.reviews {
+    text-align: center;
+    background: #fff3e0;
+    padding: 50px 20px;
+}
+.reviews .review {
+    max-width: 600px;
+    margin: 20px auto;
+    background: #fff;
+    padding: 20px;
+    border-left: 5px solid #ff8800;
+    border-radius: 10px;
+
+}
+
+.reviews h2 {
+    color: #ff8800;
+}
 </style>
 
 <main>
