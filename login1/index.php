6401596dad4ab43ef5e8afa95cac3fd5da633fbc
Subash Chhetri <subash23000@student.hamk.fi>
2025-02-15 11:55:36 +0200

    done

--- login1/index.php
+++ login1/index.php
@@ -1,11 +1,23 @@
+<?php
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My Website</title>
 </head>
 <body>
+
+
+    <a href="logout.php">Logout</a>
+    <h1>This is the index page</h1>
+
+    <br>
+    Hello, Username
     
 </body>
 </html>
