41f0e42af2c24f9092034465533e623b1ec80873
Subash Chhetri <subash23000@student.hamk.fi>
2025-02-15 14:08:06 +0200

    a

--- login1/signup.php
+++ login1/signup.php
@@ -16,7 +16,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
         // save to database
         $user_id = random_num(20);
         $query = "insert into users (user_id,user_name,password) values ('$user_id',$'user_name','$password')";
-        mysqli_query($con,$query);
+        mysqli_query($conn,$query);
 
         header("Location: login.php");
         die;
